6a3442cb63a15ff234b95cc14a58f7bc518d842c
Nan John <93335989+theflash010@users.noreply.github.com>
2026-05-31 15:07:57 +0800

    normal

--- examples/multimodal/combine_state_dicts.py
+++ examples/multimodal/combine_state_dicts.py
@@ -15,11 +15,11 @@ sys.path.append(
 def combine(input_files, module_prefixes, output_files):
     num_inputs_per_output = int(len(input_files) / len(output_files))
 
-    for output_idx, output_file in enumerate(output_files):
+    for output_idx, output_file in enumerate(output_files): #每个循环处理一个分片
         combined_state_dict = None
 
-        lb = output_idx * num_inputs_per_output
-        ub = (output_idx + 1) * num_inputs_per_output
+        lb = output_idx * num_inputs_per_output #lower bound
+        ub = (output_idx + 1) * num_inputs_per_output #upper bound
         current_input_files = input_files[lb:ub]
         current_module_prefixes = module_prefixes[lb:ub]
 
@@ -30,18 +30,18 @@ def combine(input_files, module_prefixes, output_files):
             # NOTE: To load legacy checkpoints, set TORCH_FORCE_NO_WEIGHTS_ONLY_LOAD=1
             # (only use with trusted files — allows arbitrary code execution).
             current_state_dict = torch.load(input_file)
-            if i == 0:
+            if i == 0: #对于第一个分片的第一个组件language_model，创建combined_state_dict
                 combined_state_dict = current_state_dict.copy()
                 combined_state_dict["model"] = dict()
 
             # copy model state dict and prefix names with the given module keys.
-            for k, v in current_state_dict["model"].items():
+            for k, v in current_state_dict["model"].items(): #给当前组件的state_dict的key添加前缀，就是重命名来符合Megatron多模态格式
                 combined_state_dict["model"]["%s.%s" % (module_prefix, k)] = v
 
         output_dir = os.path.dirname(output_file)
         if not os.path.exists(output_dir):
             os.makedirs(output_dir, exist_ok=True)
-        torch.save(combined_state_dict, output_file)
+        torch.save(combined_state_dict, output_file) #利用torch.save保存combined参数权重
         print("saved:", output_file)
 
     print("done.")
@@ -67,7 +67,7 @@ if __name__ == "__main__":
         nargs="*",
         required=True,
         help="prefixes to use with each input model's parameters",
-    )
+    ) #前缀就是language_model和vision_model
     parser.add_argument(
         "--output", nargs="*", required=True, help="path(s) to output state dict file"
     )
